62a79d6ae088d6c6de53a3bcdf13379a3c408aeb
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2023-02-11 20:04:20 +0100

    Add prefetch?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,28 @@
 import { loadEnv } from "vite";
-
 import { defineConfig } from "astro/config";
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
-import node from "@astrojs/node";
-
-const { PUBLIC_SITE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+import prefetch from "@astrojs/prefetch";
+const {
+  PUBLIC_SITE_URL
+} = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: PUBLIC_SITE_URL,
   trailingSlash: "always",
-  integrations: [
-    tailwind(),
-    mdx(),
-    svelte(),
-    sitemap({
-      i18n: {
-        defaultLocale: "nl",
-        locales: {
-          en: "en",
-          nl: "nl",
-        },
-      },
-    }),
-  ],
+  integrations: [tailwind(), prefetch(), mdx(), svelte(), sitemap({
+    i18n: {
+      defaultLocale: "nl",
+      locales: {
+        en: "en",
+        nl: "nl"
+      }
+    }
+  })],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
