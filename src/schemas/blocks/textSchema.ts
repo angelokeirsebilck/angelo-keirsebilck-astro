bb9de42249d3588eb6ffcbcbdf1b06d3aece83dc
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2023-03-04 13:52:55 +0100

    404 + mail succes page

--- src/schemas/blocks/textSchema.ts
+++ src/schemas/blocks/textSchema.ts
@@ -4,4 +4,6 @@ import { bgColor } from "../fields/bgColor";
 export const textSchema = z.object({
   title: z.string(),
   bgColor,
+  textCenter: z.boolean().default(true),
+  mt0: z.boolean().default(false),
 });
